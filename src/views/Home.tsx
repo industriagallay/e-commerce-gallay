6cb3a141f122e5bd7d6ff7d8c7e29368c4bd8815
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-06-23 21:20:37 -0300

    css-iconshomepage

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,24 +1,57 @@
 import React from "react";
-import yunqueHerreroMP4 from "../assets/yunque-herrero.mp4"
-import "./Home.css"
+import yunqueHerreroMP4 from "../assets/yunque-herrero.mp4";
+import "./Home.css";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <main id="hero">
-    <div className="promo">
-        <h1>¿Estas listo para unas vacacioones
-            increibles?</h1>
-            <p> ALOJAMIENTO, VUELOS, EQUIPAJE Y MUCHO MAS</p>
-            <button type="button">EMPIEZA HOY</button>
+    <div>
+      <header>
+        <a href="" className="brand">
+          Gallay
+        </a>
+        <div className="navigation">
+          <div className="navigation-items">
+            <a href="">Home</a>
+            <a href="">Products</a>
+            <a href="">Help</a>
+            <a href="">About</a>
+          </div>
         </div>
-        <video muted autoPlay loop>  
-        <source src={yunqueHerreroMP4} type='video/mp4'/>
-        </video>
+      </header>
+      <section className="home">
+        <div className="content">
+          <h1>
+            Industria<span>Gallay</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
+            unde et autem ipsam voluptates veniam blanditiis adipisci quam
+            tempora{" "}
+          </p>
+          <a href="#">Iniciar Sesion</a>
+        </div>
+        <div className="media-icons">
+          <a href=" ">
+            <i className="bi bi-facebook"></i>
+          </a>
+          <a href="">
+            <i className="bi bi-instagram"></i>
+          </a>
+          <a href="">
+            <i className="bi bi-youtube"></i>
+          </a>
+          {/* <a href=""> <i className="fab fa-facebook"></i></a>
+          <a href=""> <i className="fab fa-youtube"></i></a>
+          <a href=""> <i className="fab fa-instagram"></i></a> */}
+        </div>
+      </section>
+      <div></div>
+      <video muted autoPlay loop>
+        <source src={yunqueHerreroMP4} type="video/mp4" />
+      </video>
       <div className="capa"></div>
-</main>
-  )
-}
+    </div>
+  );
+};
 
 export default Home;
-
-
