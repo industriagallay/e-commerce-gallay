84da31b53a6bda3d9c6e82f5361455c69e10a2c9
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-06-28 18:25:43 -0300

    add-ts-paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-plugin-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+});
