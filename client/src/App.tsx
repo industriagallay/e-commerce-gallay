00645d22b118156f1605a240d4ae7e09a9818452
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-07-05 22:02:25 -0300

    landingpageBootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-import ErrorPage from "./components/routes/ErrorPage";
+
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Products from "./components/Products";
 import Help from "./components/Help";
@@ -15,11 +14,10 @@ const App = () => {
          
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/products" element={<Products />} />
         <Route path="/help" element={<Help />} />
-        <Route path="*" element={<ErrorPage />} />
+       
       </Routes>
     </>
   );
