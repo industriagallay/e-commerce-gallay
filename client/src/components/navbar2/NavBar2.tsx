45643c481f5e284fce878ce64d885a9db04654ed
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-07-12 22:11:00 -0300

    componente-Help

--- client/src/components/navbar2/NavBar2.tsx
+++ client/src/components/navbar2/NavBar2.tsx
@@ -6,73 +6,75 @@ import "./NavBar2.css";
 
 const NavBar2 = () => {
   return (
-    <div className="container-fluid navbar-container">
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <nav className="navbar2 bg-dark navbar-expand-md navbar-light">
-          <Link className="navbar2-logo" aria-current="page" to="/">
-            <GiRocketThruster className="navbar2-icon" />
-            Gallay
-          </Link>
+    <div>
+      <div className="navbar-container">
+        <IconContext.Provider value={{ color: "#fff" }}>
+          <nav className=" navbar2 bg-dark navbar-expand-md navbar-light container-fluid">
+            <Link className="navbar2-logo" aria-current="page" to="/">
+              <GiRocketThruster className="navbar2-icon" />
+              Gallay
+            </Link>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar2-toggler-icon"></span>
-          </button>
-
-          <div className="navbar2-links-container">
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              <ul className="navbar2-links navbar-nav ms-auto me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <Link
-                    to="/home"
-                    className="nav-link active"
-                    aria-current="page"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="/products"
-                    className="nav-link active"
-                    aria-current="page"
-                  >
-                    Products
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="/help"
-                    className="nav-link active"
-                    aria-current="page"
-                  >
-                    Help
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    to="/signup"
-                    className="nav-link active"
-                    aria-current="page"
-                  >
-                    Signup
-                  </Link>
-                </li>
-              </ul>
+              <span className="navbar2-toggler-icon"></span>
+            </button>
+
+            <div className="navbar2-links-container">
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar2-links navbar-nav ms-auto me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link
+                      to="/home"
+                      className="nav-link active"
+                      aria-current="page"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/products"
+                      className="nav-link active"
+                      aria-current="page"
+                    >
+                      Products
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/help"
+                      className="nav-link active"
+                      aria-current="page"
+                    >
+                      Help
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/signup"
+                      className="nav-link active"
+                      aria-current="page"
+                    >
+                      Signup
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
-          </div>
-        </nav>
-      </IconContext.Provider>
+          </nav>
+        </IconContext.Provider>
+      </div>
     </div>
   );
 };
