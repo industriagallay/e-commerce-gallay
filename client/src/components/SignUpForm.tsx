c754971defeaafaa8660d01b9e6bd209a842ec9c
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-07-15 19:31:54 -0300

    estilos/globales-actualizados/scrollentodaLaPagina

--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -54,10 +54,10 @@ const SignUpForm: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                   >
-                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                    <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
                     <g
-                      stroke-linejoin="round"
-                      stroke-linecap="round"
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
                       id="SVGRepo_tracerCarrier"
                     ></g>
                     <g id="SVGRepo_iconCarrier">
@@ -65,8 +65,8 @@ const SignUpForm: React.FC = () => {
                       <path
                         fill="#ffffff"
                         d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z"
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
                       ></path>{" "}
                     </g>
                   </svg>
@@ -78,10 +78,10 @@ const SignUpForm: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                   >
-                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                    <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
                     <g
-                      stroke-linejoin="round"
-                      stroke-linecap="round"
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
                       id="SVGRepo_tracerCarrier"
                     ></g>
                     <g id="SVGRepo_iconCarrier">
@@ -89,8 +89,8 @@ const SignUpForm: React.FC = () => {
                       <path
                         fill="#ffffff"
                         d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z"
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
                       ></path>{" "}
                     </g>
                   </svg>
@@ -102,10 +102,10 @@ const SignUpForm: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                   >
-                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                    <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
                     <g
-                      stroke-linejoin="round"
-                      stroke-linecap="round"
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
                       id="SVGRepo_tracerCarrier"
                     ></g>
                     <g id="SVGRepo_iconCarrier">
@@ -113,8 +113,8 @@ const SignUpForm: React.FC = () => {
                       <path
                         fill="#ffffff"
                         d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z"
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
                       ></path>{" "}
                     </g>
                   </svg>
@@ -126,10 +126,10 @@ const SignUpForm: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                   >
-                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                    <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
                     <g
-                      stroke-linejoin="round"
-                      stroke-linecap="round"
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
                       id="SVGRepo_tracerCarrier"
                     ></g>
                     <g id="SVGRepo_iconCarrier">
@@ -137,8 +137,8 @@ const SignUpForm: React.FC = () => {
                       <path
                         fill="#ffffff"
                         d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z"
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
                       ></path>{" "}
                     </g>
                   </svg>
@@ -150,10 +150,10 @@ const SignUpForm: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                   >
-                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                    <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
                     <g
-                      stroke-linejoin="round"
-                      stroke-linecap="round"
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
                       id="SVGRepo_tracerCarrier"
                     ></g>
                     <g id="SVGRepo_iconCarrier">
@@ -161,8 +161,8 @@ const SignUpForm: React.FC = () => {
                       <path
                         fill="#ffffff"
                         d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z"
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
                       ></path>{" "}
                     </g>
                   </svg>
@@ -174,10 +174,10 @@ const SignUpForm: React.FC = () => {
                     fill="none"
                     viewBox="0 0 24 24"
                   >
-                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                    <g strokeWidth="0" id="SVGRepo_bgCarrier"></g>
                     <g
-                      stroke-linejoin="round"
-                      stroke-linecap="round"
+                      strokeLinejoin="round"
+                      strokeLinecap="round"
                       id="SVGRepo_tracerCarrier"
                     ></g>
                     <g id="SVGRepo_iconCarrier">
@@ -185,8 +185,8 @@ const SignUpForm: React.FC = () => {
                       <path
                         fill="#ffffff"
                         d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z"
-                        clip-rule="evenodd"
-                        fill-rule="evenodd"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
                       ></path>{" "}
                     </g>
                   </svg>
